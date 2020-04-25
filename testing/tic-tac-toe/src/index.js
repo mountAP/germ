260e46104fecc1304a442bb6c43b3a89e2c751f4
mountAP <ap44122@gmail.com>
2020-04-25 20:01:45 +0530

    task5:silly-story-generator

--- testing/tic-tac-toe/src/index.js
+++ testing/tic-tac-toe/src/index.js
@@ -81,7 +81,6 @@ class Game extends React.Component {
   }
 
   render() {
-
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
